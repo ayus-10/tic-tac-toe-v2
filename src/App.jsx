9c2a26c18fab6c6fe021a8946dcd65d2e20a611c
Aayush Upreti <136055230+ayus-10@users.noreply.github.com>
2024-01-09 16:40:49 +0545

    fixed gameboard

--- src/App.jsx
+++ src/App.jsx
@@ -3,37 +3,40 @@ import "./App.css";
 
 const App = () => {
   const zeroArray = () => {
-    return Array.from({ length: 9 }).fill(0);
+    return Array.from({ length: 9 }).fill("0");
+  };
+
+  const updateState = (prevState, index, value) => {
+    let newState = [...prevState];
+    newState[index] = value;
+    return newState;
   };
 
   const [xState, setXState] = useState(zeroArray());
   const [oState, setOState] = useState(zeroArray());
+  const [gameBoard, setGameBoard] = useState(Array.from({ length: 9 })); // redundancy
 
-  const [playerCount, setPlayerCount] = useState(1);
+  const [turn, changeTurn] = useState(1);
 
   const handleClick = (index) => {
-    setPlayerCount((playerCount) => 1 - playerCount);
+    changeTurn((turn) => 1 - turn);
 
-    const updateState = (prevState) => {
-      let newState = [...prevState];
-      newState[index] = 1;
-      return newState;
-    };
-
-    if (playerCount === 1) {
-      setXState(updateState);
-    } else if (playerCount === 0) {
-      setOState(updateState);
+    if (turn === 1) {
+      setXState((prevState) => updateState(prevState, index, "1"));
+    } else if (turn === 0) {
+      setOState((prevState) => updateState(prevState, index, "1"));
     }
   };
 
   useEffect(() => {
-    console.log(xState);
-  }, [xState]);
-
-  useEffect(() => {
-    console.log(oState);
-  }, [oState]);
+    gameBoard.forEach((_, index) => {
+      if (xState[index] === "1") {
+        setGameBoard((prevState) => updateState(prevState, index, "X"));
+      } else if (oState[index] === "1") {
+        setGameBoard((prevState) => updateState(prevState, index, "O"));
+      }
+    });
+  }, [xState, oState]);
 
   return (
     <div className="container">
@@ -45,13 +48,7 @@ const App = () => {
               className="box"
               onClick={() => handleClick(index)}
             >
-              {playerCount === 1
-                ? xState[index] === 1
-                  ? "X"
-                  : null
-                : oState[index] === 1
-                ? "O"
-                : null}
+              {gameBoard[index]}
             </button>
           );
         })}
