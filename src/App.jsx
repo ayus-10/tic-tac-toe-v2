453555384da1a0806028c084046b525908b45dde
Aayush Upreti <136055230+ayus-10@users.noreply.github.com>
2024-01-09 17:32:20 +0545

    added winner logic

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 
 const App = () => {
-  const zeroArray = () => {
-    return Array.from({ length: 9 }).fill("0");
+  const getArray = (elements) => {
+    return Array.from({ length: 9 }).fill(elements);
   };
 
   const updateState = (prevState, index, value) => {
@@ -12,9 +12,9 @@ const App = () => {
     return newState;
   };
 
-  const [xState, setXState] = useState(zeroArray());
-  const [oState, setOState] = useState(zeroArray());
-  const [gameBoard, setGameBoard] = useState(Array.from({ length: 9 })); // redundancy
+  const [xState, setXState] = useState(getArray(0));
+  const [oState, setOState] = useState(getArray(0));
+  const [gameBoard, setGameBoard] = useState(getArray(null));
 
   const [turn, changeTurn] = useState(1);
 
@@ -22,10 +22,36 @@ const App = () => {
     changeTurn((turn) => 1 - turn);
 
     if (turn === 1) {
-      setXState((prevState) => updateState(prevState, index, "1"));
+      if (xState[index] !== "1" && oState[index] !== "1") {
+        setXState((prevState) => updateState(prevState, index, "1"));
+      }
     } else if (turn === 0) {
-      setOState((prevState) => updateState(prevState, index, "1"));
+      if (xState[index] !== "1" && oState[index] !== "1") {
+        setOState((prevState) => updateState(prevState, index, "1"));
+      }
     }
+  };
+
+  const checkWinner = () => {
+    const wins = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    wins.forEach((win, index) => {
+      if (xState[win[0]] + xState[win[1]] + xState[win[2]] === "111") {
+        alert("X Won");
+        location.reload();
+      } else if (oState[win[0]] + oState[win[1]] + oState[win[2]] === "111") {
+        alert("O Won");
+        location.reload();
+      }
+    });
   };
 
   useEffect(() => {
@@ -36,6 +62,8 @@ const App = () => {
         setGameBoard((prevState) => updateState(prevState, index, "O"));
       }
     });
+
+    checkWinner();
   }, [xState, oState]);
 
   return (
